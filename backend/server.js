065abb23421fe04d4ebfcbb4231541f5f7026889
Simson <simsont.wwi21@student.dhbw-heidenheim.de>
2024-04-30 10:01:15 +0200

    minor adjustments to server.js --> added cors connection

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,18 @@ require('dotenv').config();
 // server.js
 const express = require('express');
 const cors = require('cors');
-
 const { connect } = require('./database'); // Pfad zur database.js-Datei
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // Passen Sie dies an die URL Ihres Frontends an
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions)); // CORS für alle Routen verwenden
+app.use(express.json()); // Ermöglicht das Parsen von JSON-body Requests
 
 // Verbindung zur Datenbank herstellen, bevor der Server startet
 connect().then(db => {
